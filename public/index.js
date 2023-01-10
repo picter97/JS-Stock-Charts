ba8cef8304d2bae73bedf1812ba4c47ac87f1935
picter <picter2esolv@gmail.com>
2023-01-10 17:33:55 -0500

    unable to install chart

--- public/index.js
+++ public/index.js
@@ -4,6 +4,56 @@ async function main() {
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
+    let response = await fetch('https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1min&apikey=5b05c14c2b9d451c956434c09ecdd2fe')
+
+    let result = await response.json()
+    let GME = result.GME
+    let MSFT = result.MSFT
+    let DIS = result.DIS
+    let BNTX = result.BNTX
+
+    
+    //const {GME, MSFT, DIS, BNTX} = mockData;
+    
+    const stocks = [GME,MSFT,DIS,BNTX];
+    console.log(stocks)
 }
+
+//import Chart from 'chart.js/auto';
+function getColor(stock){
+    if(stock === "GME"){
+        return 'rgba(61, 161, 61, 0.7)'
+    }
+    if(stock === "MSFT"){
+        return 'rgba(209, 4, 25, 0.7)'
+    }
+    if(stock === "DIS"){
+        return 'rgba(18, 4, 209, 0.7)'
+    }
+    if(stock === "BNTX"){
+        return 'rgba(166, 43, 158, 0.7)'
+    }
+}
+stocks.forEach(stock => stock.value.reverse())
+
+var ctx = document.getElementById('myChart').getContext('2d');
+        var myChart = new Chart(timeChartCanvas.getContext('2d'), {
+    type: 'line',
+    data: {
+        labels: stocks[0].values.map(value => value.datetime),
+        datasets: stocks.map( stock =>  ({
+            label: stock.meta.symbol,
+            data: stock.values.map(value => parseFloat(value.high)),
+            backgroundColor: getColor(stock.meta.symbol),
+            borderColor: getColor(stock.meta.symbol),
+            }))
+        }
+    });
+
+
+
+
+
+
 
 main()
